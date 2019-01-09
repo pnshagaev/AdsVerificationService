642dc7fe93253a9b2316196812821e512f0ef00f
Pavel Shagaev <pnshagaev@domclick.ru>
2019-01-09 18:27:35 +0300

    model refactoring

--- src/models.py
+++ src/models.py
@@ -1,8 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
+
 db = SQLAlchemy()
 
+# Define models
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
 
-class UserRoles(db.Model):
+
+class Role(db.Model, RoleMixin):
     """
     current supported roles:
     id = 0, role = admin
@@ -10,14 +19,24 @@ class UserRoles(db.Model):
     id = 2, role = user
 
     """
-    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.Text)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    person = db.Column(db.Integer)
-    roles = db.ForeignKey(UserRoles.id)
-
+    def __str__(self):
+        return self.email
