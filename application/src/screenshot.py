9891a1b5b4d82ed62e17362ed93db15d1b616039
pavel <shagaev-pavel@mail.ru>
2019-02-11 02:10:05 +0300

    screenshot saver fix

--- application/src/screenshot.py
+++ application/src/screenshot.py
@@ -3,10 +3,11 @@ from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import shutil
 
 
 def setup_driver():
-    driver_name = 'chromedriver'
+    driver_name = '/Users/pavel/bin/chromedriver'
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1000,1500")
     chrome_options.add_argument("--headless")
@@ -16,7 +17,7 @@ def setup_driver():
 
 def save_yandex_screenshot(driver, word, path_to_save):
     driver.get(f'https://yandex.ru/search/?text={word}')
-    driver.save_screenshot('yandex_{}/{}.png'.format(path_to_save, str(word)))
+    driver.save_screenshot('{}/{}.png'.format(path_to_save, str(word)))
 
 
 def create_folder(path):
@@ -25,14 +26,16 @@ def create_folder(path):
 
 
 def search_words(words):
-    path = './' + str(datetime.datetime.now())
+    name = 'yandex_' + str(datetime.datetime.now())
+    path = './' + name
     create_folder(path)
     driver = setup_driver()
+    print(words)
     try:
         for word in words:
             save_yandex_screenshot(driver, word.rstrip(), path)
-            print(f'done: {word}')
     except FileNotFoundError:
         print(f'incorrect file path: {words_file}')
+    shutil.make_archive(name, 'zip', path)
     driver.quit()
-    return path
+    return path + '.zip'
